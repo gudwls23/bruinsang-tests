c4962c1adffb63918709df10337aee9ed3c82956
hyeongjin <gudwls23@likelion.org>
2017-07-25 00:35:55 -0700

    routing error fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'homes/index';
   get 'homes/events';
   get 'homes/index';
-  get 'homes#landingpage';
-  get 'homes#bbul';
+  get 'homes/landingpage';
+  get 'homes/bbul';
 
+  root 'homes#landingpage'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
